3c4b7df83a780ce2f0c2a34bc2469e8cef187784
PythonNut <PythonNut@users.noreply.github.com>
2020-07-13 00:23:05 -0700

    Adapt to update frecent dir zstyle changes in zsh-autocomplete

--- modules/autocomplete.zsh
+++ modules/autocomplete.zsh
@@ -6,6 +6,7 @@ zstyle ':autocomplete:list-choices:*' max-lines 10
 zstyle ':autocomplete:*' magic off
 zstyle ':autocomplete:tab:*' completion select
 zstyle ':autocomplete:' frecent-dirs off
+zstyle ':autocomplete:' frecent-files off
 zstyle ':autocomplete:*:no-matches-yet' message '...'
 zstyle ':autocomplete:*:no-matches-at-all' message "- no matches -"
 zstyle ':autocomplete:*:too-many-matches' message '---'
